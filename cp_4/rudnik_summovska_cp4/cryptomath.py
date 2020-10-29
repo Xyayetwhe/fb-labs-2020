ed1cfc1987e42d2ae09a677c3fbfcbd3cc2f9a43
Xyayetwhe <diamodgolem116@gmail.com>
2020-10-30 00:09:15 +0200

    miller rabin and choose random prime

---
+++ cp_4/rudnik_summovska_cp4/cryptomath.py
@@ -0,0 +1,54 @@
+import random
+import math
+prime_from = int('01'+'0'*512,2)
+prime_to = int('1'+'1'*512,2)
+
+
+def gcd(x,y):
+    while (y):
+        x,y=y,x%y
+    return x
+
+
+def miller_rabin(p):
+    for number in [2,3,5,7]:
+        if gcd(number,p) != 1 or pow(number,p-1,p) != 1:
+            return False
+    d = p - 1
+    s = 0
+    alredy_chosen = []
+    while d % 2 == 0:
+        d //= 2
+        s += 1
+    for counter in range(10):
+        x = 0
+        while True:
+            x = random.randint(1,p-1)
+            if x not in alredy_chosen:
+                break
+        if gcd(x,p) > 1:
+            return False
+        if pow(x,d,p) == 1 or pow(x,d,p) == p-1:
+            continue
+        else:
+            pseudo_random = False
+            for r in range(1,s):
+                x_r = pow(x,d * 2**r,p)
+                if x_r == p-1:
+                    pseudo_random = True
+                elif x_r == 1:
+                    return False
+                else:
+                    continue
+            if not pseudo_random:
+                return False
+    return True
+
+
+def choose_random_prime(n0=prime_from,n1=prime_to):
+    x = random.randint(n0,n1)
+    m0 = x if x % 2 != 0 else x + 1
+    for i in range(1,n1-m0//2):
+        candidate = m0+2*i
+        if miller_rabin(candidate):
+            return candidate
